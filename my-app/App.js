a6e5ac8fa109f72fdb9c84f473b77102c7f0c0d9
github_Venessa <vasiedu010@st.ug.edu.gh>
2024-06-01 09:33:41 +0000

    Added the search image

--- my-app/App.js
+++ my-app/App.js
@@ -1,11 +1,12 @@
  import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.boldText}>Hello , Devs</Text>
-      <Text styles={styles.text}>15 tasks today</Text>
+      <Text style={styles.text}>15 tasks today</Text>
+      <Image source={require('./assets/Search.png')} style={styles.image} />
     </View>
   );
 }
@@ -24,5 +25,10 @@ const styles = StyleSheet.create({
   },
   text: {
     marginBottom: 5,
-  }
+  },
+  image: {
+    width: 350,
+    height: 49,
+    marginTop: 10,
+  },
 });
